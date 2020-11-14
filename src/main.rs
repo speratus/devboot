5097820b80260c2ce14d0483d2bb16b14a45c890
Andrew Luchuk <andrew.luchuk@outlook.com>
2020-11-14 16:33:52 -0500

    adds get_config_dir function

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,20 @@
+use std::path::PathBuf;
+
 mod environ;
 mod cmdline;
+
+use dirs_next;
+
+pub fn get_config_dir() -> PathBuf {
+    let mut home_dir;
+    if let Some(dir) = dirs_next::home_dir() {
+        home_dir = dir;
+    } else {
+        panic!("User home directory doesn't exist!")
+    }
+    home_dir.push(".devboot");
+    home_dir
+}
 
 fn main() {
     println!("Hello, world!");
